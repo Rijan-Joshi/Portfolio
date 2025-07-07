a1a1208cc369c86a1b1ac4855a3fd55be5d780ae
Rijan-Joshi <csaijspy@gmail.com>
2025-07-07 11:32:36 +0545

    Phase 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -203,7 +203,7 @@ export default function Page() {
                 Contact
               </div>
               <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                'Hi' is enough
+                &apos;Hi&apos; is enough
               </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 Leave your trace on{" "}
