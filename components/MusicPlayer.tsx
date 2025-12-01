2b6360b1ffe4690626601e8cd2333e2eb27001b7
Rijan-Joshi <csaijspy@gmail.com>
2025-12-01 23:38:25 +0545

    Music path changes

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -6,27 +6,27 @@ import Tooltip from "./Tooltip";
 const TRACKS = [
   {
     title: "When we feel young",
-    src: "/music/When_we_feel_young.mp3",
+    src: "https://github.com/Rijan-Joshi/Portfolio/blob/main/music/When_we_feel_young.mp3",
   },
   {
     title: "Resham Phiriri",
-    src: "/music/Resham_Phiriri.mp3",
+    src: "https://github.com/Rijan-Joshi/Portfolio/blob/main/music/Resham_Phiriri.mp3",
   },
   {
     title: "Three Little Birds",
-    src: "/music/3_little_birds.mp3",
+    src: "https://github.com/Rijan-Joshi/Portfolio/blob/main/music/3_little_birds.mp3",
   },
   {
     title: "Ob La Di Ob La Da",
-    src: "/music/ObLaDiObLaDa.mp3",
+    src: "https://github.com/Rijan-Joshi/Portfolio/blob/main//music/ObLaDiObLaDa.mp3",
   },
   {
     title: "Deurali ma batas chalyo",
-    src: "/music/Deurali_ma_batas_chalyo.mp3",
+    src: "https://github.com/Rijan-Joshi/Portfolio/blob/main/music/Deurali_ma_batas_chalyo.mp3",
   },
   {
     title: "No Sunshine",
-    src: "/music/No_Sunshine.mp3",
+    src: "https://github.com/Rijan-Joshi/Portfolio/blob/main/music/No_Sunshine.mp3",
   },
 ];
 
